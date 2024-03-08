9baa199f0786e220707aa400f0e5689faa38d6f9
Rajni <157015217+Tech-Savvy-Rajni@users.noreply.github.com>
2024-03-08 10:01:08 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String inputCsvfile = "C:/Users/rajni/Desktop/input/test1.csv";
-        String outputCsvfile = "C:/Users/rajni/Desktop/output/test1.csv";
+        String inputCsvfile = "C:/Users/abc/Desktop/input/test1.csv";
+        String outputCsvfile = "C:/Users/abc/Desktop/output/test1.csv";
         CSVReader.read(inputCsvfile);
 //        CSVFileCopier.copyPaste(inputCsvfile,outputCsvfile);
-        CSVWriter.write(outputCsvfile,"9541310605","Rajni Sharma");
+        CSVWriter.write(outputCsvfile,"12345678","Rajni Sharma");
 //        APIRequest.APIRequest();
 //        APIRequestHttps.APIRequest();
 //        try{
